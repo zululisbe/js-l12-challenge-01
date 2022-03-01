7c9d61d6f49f5c83ff106d4b6cc04eceef8580b7
zoeestewart <zuluoscarecho@gmail.com>
2022-03-01 11:39:16 +0300

    Added random image and click button

--- js/script.js
+++ js/script.js
@@ -3,11 +3,34 @@ const authorSpan = document.querySelector(".author");
 const imgDiv = document.querySelector(".image-container");
 const img = document.querySelector(".img");
 
+// Add the API
 const getImage = async function() {
     const res = await fetch (
         "https://picsum.photos/v2/list?limit=100"
     );
     const images = await res.json();
     console.log(images);
+    selectRandomImage(images);
 };
-getImage();
+
+// Random image function
+const selectRandomImage = function (images) {
+    const randomIndex = Math.floor(Math.random() * images.length);
+    //console.log(randomIndex);
+    const randomImage = images [randomIndex];
+    displayImage(randomImage);
+};
+
+//Display random image
+const displayImage = function (randomImage) {
+   const author =  randomImage.author;
+   const imageAddress = randomImage.download_url;
+   authorSpan.innerText = author;
+   img.src = imageAddress;
+   imgDiv.classList.remove("hide");
+};
+
+//Event listener for button 
+button.addEventListener("click", function () {
+    getImage();
+});
